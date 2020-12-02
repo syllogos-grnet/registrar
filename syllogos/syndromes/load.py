7cb9919c60b4e9542d2b79a5dcdd16fbbe1c6a7c
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2020-12-02 21:18:35 +0200

    Implement load of Registar model from excel

--- syllogos/syndromes/load.py
+++ syllogos/syndromes/load.py
@@ -1,11 +1,16 @@
 import json
 
 import pandas
+from dateutil.parser import isoparse
+from django.utils.timezone import datetime
+from pytz import utc
+
+from syndromes.models import BIG_BANG, Registar
 
 
 def xlsx_to_records(input_file_path: str, filter_func=None) -> list:
     data_frame = pandas.read_excel(input_file_path)
-    data_str = data_frame.to_json(orient='records')
+    data_str = data_frame.to_json(orient='records', date_format='iso')
     for record in json.loads(data_str):
         if filter_func and not filter_func(record):
             continue
@@ -16,7 +21,51 @@ def only_active(record: list) -> bool:
     return record.get('Κατάσταση') in ['ΕΝΕΡΓΟ', 'ΕΝΕΡΓΟ-ΑΠΟΧΩΡΗΣΗ']
 
 
+def calculate_dept(record, subscription_date):
+    dept = 10.0 - float(record.get('Εγγραφή.1', 0))
+    installments = [k for k in record if 'Δόση' in k]
+    installment_date, installment_dept = dict(), dict()
+    for i in installments:
+        year = i[-4:]
+        month = '01' if i[0] == '1' else '06'
+        date_str = f'01/{month}/{year}EET'
+        installment_date[i] = datetime.strptime(date_str, '%d/%m/%Y%Z')
+        installment_dept[i] = 10.0 if int(year) < 2018 else 5.0
+    for i in installments:
+        tzinfo = installment_date[i].tzinfo
+        if utc.localize(installment_date[i]) > subscription_date and (
+                installment_date[i] < datetime.now(tzinfo)):
+            payed = float(record.get(i, 0) or 0)
+            dept += installment_dept[i] - payed
+    return dept
+
+
+def get_date(date_record):
+    if date_record:
+        return isoparse(date_record)
+    return utc.localize(BIG_BANG)
+
+
+def reload_registar(xlsx_file_path: str) -> bool:
+    Registar.objects.all().delete()
+    for record in xlsx_to_records(xlsx_file_path, only_active):
+        registar_id = int(record['Α/Α'])
+        subscription_date = get_date(record['Εγγραφή'])
+        name = ' '.join([record.get('Όνομα', '-'), record.get('Επίθετο', '-')])
+        email = record['PR']
+        dept = calculate_dept(record, subscription_date)
+        Registar(
+            register_id=registar_id,
+            subscription_date=subscription_date,
+            name=name,
+            email=email,
+            dept=dept
+        )
+
+
 if __name__ == '__main__':
     input_file_path = input('Input file path: ')
     output = list(xlsx_to_records(input_file_path, only_active))
     print(json.dumps(output, indent=2, ensure_ascii=False))
+    print()
+    # reload_registar(input_file_path)
--- syllogos/syndromes/models.py
+++ syllogos/syndromes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+BIG_BANG = timezone.datetime.strptime('20150101EET', '%Y%m%d%Z')
+
+
+class Registar(models.Model):
+    registar_id = models.IntegerField(primary_key=True)
+    subscription_date = models.DateField(null=False, default=BIG_BANG)
+    name = models.CharField(max_length=1024, null=False)
+    email = models.EmailField()
+    dept = models.FloatField(default=0.0)
