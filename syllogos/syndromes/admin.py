7f9f4a806377a1daa944080b06368251ac47b9f8
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2020-12-08 18:38:02 +0200

    Create admin UI to manage registration dept.

--- syllogos/syndromes/admin.py
+++ syllogos/syndromes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from syndromes.models import Registar
+
 # Register your models here.
+# admin.site.register(Registar)
+
+
+@admin.register(Registar)
+class RegistarAdmin(admin.ModelAdmin):
+    list_display = (
+        'registar_id', 'name', 'dept', 'email', 'subscription_date')
+    list_editable = ('dept', )
