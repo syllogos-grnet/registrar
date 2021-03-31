62c7233317005d2ed64fbee673c504c41f12607e
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2021-03-31 14:30:21 +0300

    Fix misspelled

--- syllogos/syllogos/settings.py
+++ syllogos/syllogos/settings.py
@@ -137,5 +137,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.environ.get('SECUREDL_STATIC_ROOT') or '.'
+STATIC_ROOT = os.environ.get('SYLLOGOS_STATIC_ROOT') or '.'
 STATIC_URL = '/static/'
