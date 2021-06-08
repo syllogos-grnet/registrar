2c890dec24849fdfabbb368b8c0c7f3e0f448a29
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2021-06-08 16:10:33 +0300

    Show notification log on admin dashboard

--- syllogos/syndromes/admin.py
+++ syllogos/syndromes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from syndromes.models import Registar
+from syndromes.models import NotificationLog, Registar
 
 # Register your models here.
 # admin.site.register(Registar)
@@ -11,3 +11,8 @@ class RegistarAdmin(admin.ModelAdmin):
     list_display = (
         'registar_id', 'name', 'dept', 'email', 'subscription_date')
     list_editable = ('dept', )
+
+
+@admin.register(NotificationLog)
+class NotificationLogAdmin(admin.ModelAdmin):
+    list_display = ('email', 'timestamp', 'registar', 'description', )
